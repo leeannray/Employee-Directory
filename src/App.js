e3f430429660ca7ad84dbeb94aae0f0c46c55cba
Lee Ann Norman <leeann.norm@gmail.com>
2020-05-12 17:24:02 -0500

    updated App.js file in src folder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import UserCard from "./components/UserCards/index.js";
+import Wrapper from "./components/Wrapper/index.js";
+import Title from "./components/Title/index.js";
+import users from "./users.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.Users to the users json array
+  state = {
+    users,
+  };
+
+  removeUser = (id) => {
+    // Filter this.state.users for users with an id not equal to the id being removed
+    const users = this.state.users.filter((user) => user.id !== id);
+    // Set this.state.users equal to the new users array
+    this.setState({ users });
+  };
+
+  // Map over this.state.users and render a UserCard component for each user object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Users List</Title>
+        {this.state.users.map((user) => (
+          <UserCard
+            removeUser={this.removeUser}
+            id={user.id}
+            key={user.id}
+            name={user.name}
+            image={user.image}
+            title={user.title}
+            department={user.department}
+            manager={user.manager}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
