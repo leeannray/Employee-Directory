36ea9e72841bf580cd554173a565a6ae8e594af2
Lee Ann Norman <leeann.norm@gmail.com>
2020-05-12 17:09:05 -0500

    created index.js file in UserCards

--- src/components/UserCards/index.js
+++ src/components/UserCards/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function UserCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Title:</strong> {props.title}
+          </li>
+          <li>
+            <strong>Department:</strong> {props.department}
+          </li>
+          <li>
+            <strong>Manager:</strong> {props.manager}
+          </li>
+        </ul>
+      </div>
+      <span className="remove">𝘅</span>
+    </div>
+  );
+}
+
+export default UserCard;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
