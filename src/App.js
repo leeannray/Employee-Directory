7458cf53582043bda6f4cdd585184cf66bfd216d
Lee Ann Norman <leeann.norm@gmail.com>
2020-05-14 17:56:05 -0500

    used component App rather than HTML elements in JSX

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ import UserCard from "../src/components/UserCard/UserCard";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <UserCard />
-      </header>
-    </div>
-  );
+  return <UserCard />
 }
 
 export default App;
