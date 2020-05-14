f54a5d97323caca426b662b38891838ee89007b9
Lee Ann Norman <leeann.norm@gmail.com>
2020-05-14 02:25:10 -0500

    condensed app.js

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -23,6 +23,72 @@ function UserCard(props) {
   );
 }
 
+import React, {Component} from 'react';
+
+class RandomUser extends Component {
+    constructor() {
+        super();
+        this.state = {
+            user: [],
+            isLoading: false,
+            error: null
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ isLoading: true });
+        this.fetchRandomUser();        
+    }
+
+    fetchRandomUser = async() => {
+        try {
+            await fetch('https://randomuser.me/api/')
+            .then(results => {
+                return results.json();
+            })
+            .then(data => {
+                let user = data.results.map((user) => {
+                    let userElm = '';
+                    
+                    if (typeof user !== undefined && typeof user.name !== undefined && typeof user.picture != undefined) {
+                        userElm = <div key={user}>
+                            <h2>{user.name.first} {user.name.last}</h2>
+                            <img src={user.picture.large} alt="" />
+                            </div>;
+                    }
+                    
+                    return(userElm)
+                });
+
+                this.setState({user: user, isLoading: false});
+            });
+        }
+        catch(error) {
+            this.setState({ error: error, isLoading: false });
+        }
+    }
+
+    render() {
+        let { user, isLoading, error } = this.state;
+
+        if (error) {
+            return <p>{error.message}</p>;
+          }
+          
+        if (isLoading) {
+            return <p>Loading...</p>;
+          }
+
+        return (
+            <div>
+                {user}
+            </div>
+        )
+    }
+}
+
+export default UserCard;
+
 
        /* /* <div className="content"></div>
         <ul>
@@ -42,4 +108,4 @@ function UserCard(props) {
       </div>
       <span onClick={() => props.removeUser(props.id)} className="remove">
         𝘅*/
-export default UserCard;
+
