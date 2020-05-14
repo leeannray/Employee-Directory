9f2e03e5ca9e902362dd9ef85032e0e8950ec389
Lee Ann Norman <leeann.norm@gmail.com>
2020-05-14 01:58:15 -0500

    final changes

--- src/App.js
+++ src/App.js
@@ -1,59 +1,161 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from "react";
+import axios from "axios";
 
-class UserCard extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-    };
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { users: [], searchTerm: "", alphabetical: "az" };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  componentWillMount() {
-    fetch("https://randomuser.me/api/?results=50")
+  componentDidMount() {
+    axios
+      .get("https://randomuser.me/api/?results=60")
       .then((response) => {
-        if (response.ok) return response.json();
-        throw new Error("Request failed.");
-      })
-      .then((data) => {
-        this.setState({ users: data.results });
+        console.log(response.data.results);
+        this.setState({ users: response.data.results });
       })
       .catch((error) => {
         console.log(error);
       });
   }
 
+  handleChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    const list = this.state.users.map((u, i) => {
-      return (
-        <User
-          key={u.login.md5}
-          name={`${u.name.first} ${u.name.last}`}
-          email={u.email}
-        />
+    let sortedUsers;
+
+    if (this.state.alphabetical === "az") {
+      console.log("sort");
+      sortedUsers = this.state.users.sort((a, b) =>
+        a.name.first > b.name.first ? 1 : -1
       );
+    } else {
+      sortedUsers = this.state.users.sort((a, b) =>
+        a.name.first < b.name.first ? 1 : -1
+      );
+    }
+
+    let filteredUsers = sortedUsers;
+
+    if (this.state.searchTerm)
+      filteredUsers = this.state.users.filter((u) =>
+        u.name.first.startsWith(this.state.searchTerm)
+      );
+
+    const userNames = filteredUsers.map((u) => {
+      return <User key={u.email} name={u.name.first} age={u.dob.age} />;
     });
     return (
       <div>
-        <h1>Current Users: </h1>
-        {list}
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Search for user:
+            <input
+              type="text"
+              name="searchTerm"
+              value={this.state.searchTerm}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <select
+          name="alphabetical"
+          value={this.state.alphabetical}
+          onChange={this.handleChange}
+        >
+          <option selected value="az">
+            A to Z
+          </option>
+          <option value="za">Z to A</option>
+        </select>
+
+        {userNames}
       </div>
     );
   }
 }
 
-class User extends React.Component {
+class User extends Component {
   render() {
     return (
-      <div style={{ borderStyle: "dotted" }}>
-        <h3>{this.props.name}</h3>
-        <p>{this.props.email}</p>
+      <div>
+        <h2>{this.props.name}</h2>
+        <h3>{this.props.age}</h3>
       </div>
     );
   }
 }
 
-ReactDOM.render(<UserCard />, document.getElementById("root"));
+export default App;
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// class UserCard extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       users: [],
+//     };
+//   }
+
+//   componentWillMount() {
+//     fetch("https://randomuser.me/api/?results=50")
+//       .then((response) => {
+//         if (response.ok) return response.json();
+//         throw new Error("Request failed.");
+//       })
+//       .then((data) => {
+//         this.setState({ users: data.results });
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }
+
+//   render() {
+//     const list = this.state.users.map((u, i) => {
+//       return (
+//         <User
+//           key={u.login.md5}
+//           name={`${u.name.first} ${u.name.last}`}
+//           email={u.email}
+//         />
+//       );
+//     });
+//     return (
+//       <div>
+//         <h1>Current Users: </h1>
+//         {list}
+//       </div>
+//     );
+//   }
+// }
+
+// class User extends React.Component {
+//   render() {
+//     return (
+//       <div style={{ borderStyle: "dotted" }}>
+//         <h3>{this.props.name}</h3>
+//         <p>{this.props.email}</p>
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<UserCard />, document.getElementById("root"));
 
 // import axios from "axios";
 // const proxyurl = "https://cors-anywhere.herokuapp.com/";
